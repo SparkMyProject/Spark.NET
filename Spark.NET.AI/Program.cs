11bd4846ec1c65519d8d0fed17c26620deaa341b
Violet Lauro <vinniehat@gmail.com>
2023-05-02 19:23:58 -0400

    Push for dev

---
+++ Spark.NET.AI/Program.cs
@@ -0,0 +1,4 @@
+﻿var api = new OpenAI_API.OpenAIAPI("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");
+var result = await api.Completions.GetCompletion("One Two Three One Two");
+Console.WriteLine(result);
+// should print something starting with "Three"
